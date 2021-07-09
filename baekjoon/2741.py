b04b854bb2ad2dc12b9395177ada4fe4ebecc68d
Taewon Kim <dev.taewon.kim@gmail.com>
2021-07-09 04:44:02 +0000

    2741 Solved

---
+++ baekjoon/2741.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Title : N 찍기
+# Link  : https://www.acmicpc.net/problem/2741
+
+from sys import stdin
+
+# 입력 빠르게 받기
+input = stdin.readline
+
+n = int(input())
+
+for i in range(n):
+    print(i + 1)
