079bb8e6d4ba65806bfe649a1b43b1c665880563
Taewon Kim <dev.taewon.kim@gmail.com>
2021-07-13 07:28:54 +0000

    1912 Solved

--- baekjoon/1912.py
+++ baekjoon/1912.py
@@ -9,4 +9,12 @@ input = stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
+dp = [0] * (n + 1)
+dp[0] = arr[0]
+result = dp[0]
 
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + arr[i], arr[i])
+    result = max(result, dp[i])
+
+print(result)
