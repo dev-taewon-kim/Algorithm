610b59f22184582936771fb9dab436815401422c
Taewon Kim <dev.taewon.kim@gmail.com>
2021-07-13 05:29:33 +0000

    1912 Solving

---
+++ baekjoon/1912.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Title : 연속합
+# Link  : https://www.acmicpc.net/problem/1912
+
+from sys import stdin
+
+# 입력 빠르게 받기
+input = stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
